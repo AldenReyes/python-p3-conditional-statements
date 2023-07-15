1804dbfd2030cdfd0c96a359ebd4684ef1b8b352
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-07-15 01:08:44 -0700

    Completes lab: explores conditional execution of code in Python

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python3
 
+
 def admin_login(username, password):
-    # your code here
-    pass
+    if username.lower() == "admin" and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature > 85:
+        return "It's too dang hot out there!"
+    elif 65 > temperature > 40:
+        return "It's a little chilly out there!"
+    elif temperature < 40:
+        return "It's brisk out there!"
+    else:
+        return "It's perfect out there!"
+
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    str = ""
+    if num % 3 == 0:
+        str += "Fizz"
+    if num % 5 == 0:
+        str += "Buzz"
+    if str == "":
+        return num
+    else:
+        return str
+
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    calc_map = {"+": num1 + num2, "-": num1 - num2, "*": num1 * num2, "/": num1 / num2}
+    result = calc_map.get(operation)
+    if result == None:
+        print("Invalid operation!")
+        return result
+    else:
+        return result
